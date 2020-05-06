051f8181697fbd9d1d014d08c6bc6212eadaffe6
alreis <al.reis@live.com>
2020-05-06 15:04:25 +0100

    Atualização

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'new_project',
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "docker",
+  database: "new_project",
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
-  },
+    underscoredAll: true
+  }
 };
